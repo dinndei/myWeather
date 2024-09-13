d107904931464d823012626416f7bc4db18f2138
dinndei <dinn0533@gmail.com>
2024-09-13 14:41:05 +0300

    last small edits

--- src/CardList.js
+++ src/CardList.js
@@ -2,21 +2,23 @@ import React from "react";
 import Card from "./Card";
 import { Grid, Container, Typography } from "@mui/material";
 const CardList = () => {
-let cityArr=["Eilat","Alaska","New York","London"];
+    let cityArr = ["Eilat", "Alaska", "New York", "London"];
     return (
-    
+
         <Container>
-        <Typography variant="h3" component="h1" align="center" gutterBottom>
-         מזג אוויר ברחבי העולם
-        </Typography>
-        <Grid container spacing={8}>
-          {cityArr.map((item, index) => (
-            <Grid item xs={12} sm={6} key={index}>
-              <Card city={item} />
+            <div>בס"ד</div>
+            <Typography variant="h3" component="h1" align="center" gutterBottom
+                sx={{ marginTop: 5, marginBottom: 4 }}>
+                מזג אוויר ברחבי העולם
+            </Typography>
+            <Grid container spacing={8}>
+                {cityArr.map((item, index) => (
+                    <Grid item xs={12} sm={6} key={index}>
+                        <Card city={item} />
+                    </Grid>
+                ))}
             </Grid>
-          ))}
-        </Grid>
-      </Container>
+        </Container>
     );
 }
 
